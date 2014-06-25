7fd5ca4e4222bfbbc70f5489d6b43c8ba7743d76
Martina <martina.malberti@cern.ch>
2014-06-25 10:33:50 +0200

    add soft killer

--- Summer14/bin/MiniNtuplizer.cpp
+++ Summer14/bin/MiniNtuplizer.cpp
@@ -145,6 +145,7 @@ double correction( PseudoJet &iJet,FactorizedJetCorrector *iJetCorr,double iRho)
 }
 
 double correctionNoL1(PseudoJet &iJet,FactorizedJetCorrector *iJetCorr,double iRho){
+  cout << "jet pt = " << iJet.pt() <<endl;
   // full correction
   iJetCorr->setJetPt (iJet.pt());
   iJetCorr->setJetEta(iJet.eta());
@@ -164,11 +165,13 @@ double correctionNoL1(PseudoJet &iJet,FactorizedJetCorrector *iJetCorr,double iR
   iJetCorr->setJetEMF(-99.0);
   std::vector<float> lCorrections = iJetCorr->getSubCorrections();
   //double jetpucorr = 1;
-  //for (unsigned int i = 0 ; i < lCorrections.size(); i++){
-  //  cout << "correction "<<i<< "  " << lCorrections[i] <<endl;}
-  //jetpucorr *= lCorrections[0];
-  //jetpucorr  = jetcorr-jetpucorr;
+  for (unsigned int i = 0 ; i < lCorrections.size(); i++){
+    cout << "correction "<<i<< "  " << lCorrections[i] <<endl;}
+  //jetpucorr *= lCorrections[0];??
+  //jetpucorr  = jetcorr-jetpucorr;??
   //cout << "full corr = " << jetcorr << "  " << (lCorrections[0]*lCorrections[1]*lCorrections[2]) <<endl;
+  cout << "full corr = " << jetcorr << endl;
+  cout << "L2L3only  = " << jetcorr/lCorrections[0] <<endl;  
   //cout << (jetpucorr) << "  " << jetcorr/lCorrections[0] <<endl;  
   return (jetcorr/lCorrections[0]); // is this correct to get only L2L3 corr?
   //return(jetcorr);
@@ -911,6 +914,11 @@ int main (int argc, char ** argv) {
   JetCleanser gsn_cleanser(subjet_def,JetCleanser::gaussian_cleansing,JetCleanser::input_nc_separate);
   gsn_cleanser.SetGaussianParameters(0.617,0.62,0.15,0.22);
 
+
+  // --- Setup soft-killer
+  SoftKiller soft_killer   (5.0,0.4);
+
+
   // --- Setup output trees
   TFile *fout = new TFile(fOut.c_str(),"RECREATE");
 
@@ -918,13 +926,16 @@ int main (int argc, char ** argv) {
   TTree *pfTree    = new TTree("pf"   , "pf"   );
   TTree *chsTree   = new TTree("chs"  , "chs"  );
   TTree *puppiTree = new TTree("puppi", "puppi");
+  TTree *softkillerTree    = new TTree("softkiller", "softkiller");
   TTree *cmsswTree = new TTree("cmsswpf", "cmsswpf");
 
-  JetInfo JGenInfo, JPFInfo, JCHSInfo, JPuppiInfo, JCMSSWPFInfo;    
+  JetInfo JGenInfo, JPFInfo, JCHSInfo, JPuppiInfo, JSoftKillerInfo, JCMSSWPFInfo;    
   setupTree(genTree,   JGenInfo    , "" );
   setupTree(pfTree,    JPFInfo     , "" );
   setupTree(chsTree,   JCHSInfo    , "" );
   setupTree(puppiTree, JPuppiInfo  , "" );
+  setupTree(softkillerTree, JSoftKillerInfo  , "" );
+
   if (doCMSSWJets) setupTree(cmsswTree, JCMSSWPFInfo, "" );
 
   
@@ -944,17 +955,21 @@ int main (int argc, char ** argv) {
     vector<PseudoJet> pf_event        = fPFCand->pfFetch();
     vector<PseudoJet> chs_event       = fPFCand->pfchsFetch(-1);
     vector<PseudoJet> puppi_event     = fPFCand->puppiFetch();
+    vector<PseudoJet> soft_event      = soft_killer(pf_event);
+
 
     // -- Cluster jets
     ClusterSequenceArea pGen    (gen_event    , jet_def, area_def);
     ClusterSequenceArea pPup    (puppi_event  , jet_def, area_def);
     ClusterSequenceArea pPF     (pf_event     , jet_def, area_def);
     ClusterSequenceArea pCHS    (chs_event    , jet_def, area_def);
+    ClusterSequenceArea pSoft   (soft_event   , jet_def,area_def);
 
     vector<PseudoJet> genJets     = sorted_by_pt(pGen    .inclusive_jets(25.));
     vector<PseudoJet> puppiJets   = sorted_by_pt(pPup    .inclusive_jets(25.));
     vector<PseudoJet> pfJets      = sorted_by_pt(pPF     .inclusive_jets(25.));
     vector<PseudoJet> chsJets     = sorted_by_pt(pCHS    .inclusive_jets(25.));
+    vector<PseudoJet> softJets    = sorted_by_pt(pSoft   .inclusive_jets(25.));
 
     lTree->GetEntry(ientry);
     int nPU = eventInfo->nPU;
@@ -964,11 +979,13 @@ int main (int argc, char ** argv) {
     fillRecoJetsInfo(puppiJets, jet_def_Pruning, puppi_event, JPuppiInfo, JGenInfo, false, jetCorr, jetUnc, applyOnlyL2L3JEC, gsn_cleanser,nPU);
     fillRecoJetsInfo(pfJets , jet_def_Pruning, pf_event   , JPFInfo   , JGenInfo, false, jetCorr, jetUnc, applyOnlyL2L3JEC, gsn_cleanser,nPU);
     fillRecoJetsInfo(chsJets, jet_def_Pruning, chs_event  , JCHSInfo  , JGenInfo, true , jetCorr, jetUnc, applyOnlyL2L3JEC, gsn_cleanser,nPU);
+    fillRecoJetsInfo(softJets, jet_def_Pruning, soft_event  , JSoftKillerInfo  , JGenInfo, true , jetCorr, jetUnc, applyOnlyL2L3JEC, gsn_cleanser,nPU);
 
     genTree->Fill();
     puppiTree->Fill();
     pfTree->Fill();
     chsTree->Fill();
+    softkillerTree->Fill();
 
     if (doCMSSWJets)
       readCMSSWJet(ientry, lTree, *cmsswTree, genJets, JCMSSWPFInfo);
@@ -983,6 +1000,7 @@ int main (int argc, char ** argv) {
   pfTree   ->Write();
   chsTree  ->Write();
   puppiTree->Write();
+  softkillerTree->Write();
 
   if (doCMSSWJets)  cmsswTree->Write();
 }  
